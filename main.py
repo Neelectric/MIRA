05eb877690cf8c4dd7c680b807d46bcde298ca8b
Neelectric <Neel.R@web.de>
2024-03-25 19:45:33 +0000

    Starting intermediate decoding

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 ### eventually, stuff will go here
+
+from utilities.intermediate_decoding import GPT2Helper
+
+gpt2_helper = GPT2Helper
+
+prompt = "The capital of Germany is"
+
+generate_text = gpt2_helper.generate_text(prompt, max_length=25)
